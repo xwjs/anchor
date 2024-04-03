234ac3b4ac5ba302e8a7e317ad3cbd1bc61862e4
xwj <2747546199@qq.com>
2024-04-04 01:23:24 +0800

    finish anchor

--- anchor.sh
+++ anchor.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/bash
 
 STORAGE_DIR='/home/cpp'
-STORAGE_FILE='.anchors'
+STORAGE_FILE='.anchor'
 declare -A ANCHORS
 declare -A g_ANCHORS
+
 
 add(){
     local anchor="$1"
     local path="$2"
     local storage="$3"
+
 
     if [ -n "${ANCHORS["$anchor"]}" ]; then
         echo "[$anchor] already exists: ${ANCHORS[$anchor]}"
@@ -18,11 +20,15 @@ add(){
             y|Y )
                 ANCHORS["$anchor"]="$path"
 
-                if [[ "$storage"==1  ]];then
+                if [ "$storage" -eq 1  ];then
                     g_ANCHORS["$anchor"]="$path"
                     echo "$1"="$2" >> ${STORAGE_DIR}/${STORAGE_FILE}
                     echo "[$anchor] -> ${ANCHORS[$anchor]} (global)"
                 else
+                    if [ -n g_ANCHORS["$anchor"]  ];then
+                        unset g_ANCHORS["$anchor"]
+                    fi
+
                     echo "[$anchor] -> ${ANCHORS[$anchor]} (local)"
                 fi
 
@@ -36,11 +42,15 @@ add(){
     else
         ANCHORS["$anchor"]="$path"
 
-        if [[ "$storage"==1  ]];then
+        if [ "$storage" -eq 1 ];then
             g_ANCHORS["$anchor"]="$path"
             echo "$1"="$2" >> ${STORAGE_DIR}/${STORAGE_FILE}
             echo "[$anchor] -> ${ANCHORS[$anchor]} (global)"
         else
+            if [ -n g_ANCHORS["$anchor"]  ];then
+                unset g_ANCHORS["$anchor"]
+            fi
+
             echo "[$anchor] -> ${ANCHORS[$anchor]} (local)"
         fi
 
@@ -50,7 +60,30 @@ add(){
 }
 
 help(){
-    echo "help"
+cat << EOF
+command:
+
+    load_anchor                         load storage file from anchors.sh's STORAGE_DIR/STORAGE_FILE
+
+    list_anchor                         show all anchor-path, red for global,white for local 
+
+    goto_anchor <anchor>                change directory to anchor points
+
+    add_anchor  <anchor> [path] [opt]   add or update anchor-path
+                                        if the arg path doesnt exist, default path is the dir runing this command
+                                        if the arg opt  doesnt exist, default visibility is local (destroyed when logged out)
+
+options:
+
+    -g                                  add global anchor (available every log in)
+
+
+EOF
+}
+
+
+anchor_help(){
+    help
 }
 
 check_anchor(){
@@ -66,6 +99,7 @@ check_path(){
     if [[ -e "$1"  ]]; then
         return 0 
     else
+        echo "path:{$1} does not exist"
         return 1
     fi
 }
@@ -83,44 +117,45 @@ parse_path(){
     local path="$1"
     local prefix="$2"
 
-    if [[ "$path" == '~'* ]]; then
+    if [[ "${path}" == '~'* ]]; then
         path="$HOME${path:1}"
-    elif [[ $path == '.' ]];
+    elif [[ "$path" == '.' ]];then
         path="${prefix}"
-    elif [[ "$path" != '/'* ]]; 
+    elif [[ "$path" != '/'* ]]; then
         path="${prefix}/${path}"
     fi
 
-    echo "$path"
+    echo "$(realpath "$path")"
 }
 
-add_anchors(){
-    if [ "$#" -e 1 ]; then
+add_anchor(){
+
+    if [ "$#" -eq 1 ]; then
         check_anchor "$1"
-        if [ "$?" -e 0 ];then
+        if [ "$?" -eq 0 ];then
             add "$1" "$(pwd)" 0
         else
             help
             return 1
         fi
 
-    elif [ "$#" -ne 2 ];
+    elif [ "$#" -eq 2 ]; then
         local path=$(parse_path "$2" "$(pwd)" )
 
         check_anchor "$1"
-        if [ "$?" -e 1 ];then
+        if [ "$?" -eq 1 ];then
             help
             return 1
         fi
 
         check_path "$path"
-        if [ "$?" -e 0 ];then
+        if [ "$?" -eq 0 ];then
             add "$1" "$path" 0
             return 0
         fi
 
         check_option "$2"
-        if [ "$?" -e 0 ];then
+        if [ "$?" -eq 0 ];then
             add "$1" "$2" 1
             return 0
         fi
@@ -129,22 +164,23 @@ add_anchors(){
 
         return 1
 
-    elif [ "$#" -ne 3 ]; 
+    elif [ "$#" -eq 3 ]; then
+
         check_anchor "$1"
-        if [ "$?" -e 1 ];then
+        if [ "$?" -eq 1 ];then
             help
             return 1
         fi
 
         local path=$(parse_path "$2" "$(pwd)" )
         check_path "$path"
-        if [ "$?" -e 1 ];then
+        if [ "$?" -eq 1 ];then
             help
             return 1
         fi
 
         check_option "$3"
-        if [ "$?" -e 1  ];then
+        if [ "$?" -eq 1  ];then
             help
             return 1
         fi
@@ -158,7 +194,7 @@ add_anchors(){
 }
 
 
-load_anchors(){
+load_anchor(){
     if [ -e "${STORAGE_DIR}/${STORAGE_FILE}" ]; then
         while IFS='=' read -r key value; do
             value=$(parse_path "$value" "$STORAGE_DIR")    
@@ -179,13 +215,15 @@ load_anchors(){
     fi
 }
 
-list_anchors() {
+list_anchor() {
+    QING='\033[1;36m'
     RED='\033[01;31m'
     GREEN='\033[01;32m'
     BLUE='\033[01;34m'
     NC='\033[0m'
 
-    # align all key
+
+
     max_length=0
     for key in "${!ANCHORS[@]}"; do
         if [ ${#key} -gt $max_length ]; then
@@ -193,16 +231,33 @@ list_anchors() {
         fi
     done
 
-    for i in "${!ANCHORS[@]}"; do
-        if [ -n "${g_ANCHORS[$i]}"  ];then
-            printf "${RED}%-${max_length}s${NC} ${GREEN}->${NC} ${BLUE}%s${NC}\n" "$i" "${ANCHORS[$i]}"
-        else
-            printf "%-${max_length}s ${GREEN}->${NC} ${BLUE}%s${NC}\n" "$i" "${ANCHORS[$i]}"
+    max_length1=1
+    for value in "${!ANCHORS[@]}"; do
+        if [ ${#value} -gt $max_length1 ]; then
+            max_length1=${#value}
         fi
     done
+
+
+    printf "${RED}Powered by anchor${NC}\n\n"
+
+
+    for i in "${!g_ANCHORS[@]}"; do
+        printf "${QING}%-${max_length}s${NC} ${GREEN}->${NC} ${BLUE}%s${NC}\n" "$i" "${g_ANCHORS[$i]}"
+    done
+
+
+    for i in "${!ANCHORS[@]}"; do
+        if [ !  -n "${g_ANCHORS[$i]}"  ];then
+           printf "%-${max_length}s ${GREEN}->${NC} ${BLUE}%s${NC}\n" "$i" "${ANCHORS[$i]}"
+        fi
+    done
+
+
 }
 
-change_dir(){
+goto_anchor(){
+
     local anchor="$1"
 
     if [ -z "$anchor" ]; then
@@ -220,8 +275,8 @@ change_dir(){
 
 }
 
-
-export -f  add_anchors
-export -f load_anchors
-export -f list_anchors
-export -f change_dir
+export -f anchor_help
+export -f  add_anchor
+export -f load_anchor
+export -f list_anchor
+export -f goto_anchor
